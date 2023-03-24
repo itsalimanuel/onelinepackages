218be6f59668c6bdac6d79bbdd4c5a68a84d1ca5
itsalimanuel <itskhaloufali@gmail.com>
2023-03-24 23:35:28 +0400

    update meta

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,11 @@ export const meta: MetaFunction = () => ({
   title: "OneLinePackages - Get All Your NPM Packages in One Line",
   description: "OneLinePackages is a website that allows you to easily retrieve all of your installed packages from the NPM registry API in just one line of code. Say goodbye to sifting through lengthy lists and manually copying package names, and hello to streamlined development with our user-friendly tool. With OneLinePackages, you can quickly and efficiently manage your NPM packages, saving you valuable time and effort.",
   viewport: "width=device-width,initial-scale=1",
+  "twitter:card": "summary_large_image",
+  "twitter:site": "@itsAliKhalouf",
+  "og:title": "OneLinePackages - Get All Your NPM Packages in One Line",
+  "og:description": "OneLinePackages is a website that allows you to easily retrieve all of your installed packages from the NPM registry API in just one line of code. Say goodbye to sifting through lengthy lists and manually copying package names, and hello to streamlined development with our user-friendly tool. With OneLinePackages, you can quickly and efficiently manage your NPM packages, saving you valuable time and effort.",
+  "og:image": './logo.svg'
 });
 
 
